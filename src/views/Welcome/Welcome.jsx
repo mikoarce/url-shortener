64e91d1742c1d39e46a122d3e432f940db1780ab
marce <mikoarce@gmail.com>
2019-04-14 17:59:56 -0400

    Transform Welcome page into a functional component.

--- src/views/Welcome/Welcome.jsx
+++ src/views/Welcome/Welcome.jsx
@@ -3,43 +3,25 @@ import { PropTypes } from 'prop-types';
 import {
   Header, Segment, Divider, Grid,
 } from 'semantic-ui-react';
-import { connect } from 'react-redux';
-import { push } from 'connected-react-router';
 import Tabs from './Tabs';
 
-export class Welcome extends React.Component {
-  static get propTypes() {
-    return {
-      dispatch: PropTypes.func.isRequired,
-    };
-  }
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: true,
-    };
-  }
+const Welcome = () => (
+<Grid verticalAlign="middle" columns={1} centered>
+  <Grid.Row>
+    <Grid.Column>
+      <Segment>
+        <Header>
+          URL Shortener and Analytics
+          <Header.Subheader>
+            Shorten your URL and view some analytics (analytics are mocked).
+          </Header.Subheader>
+        </Header>
+        <Divider />
+        <Tabs />
+      </Segment>
+    </Grid.Column>
+  </Grid.Row>
+</Grid>
+);
 
-  render() {
-    return (
-      <Grid verticalAlign="middle" columns={1} centered>
-        <Grid.Row>
-          <Grid.Column>
-            <Segment>
-              <Header>
-                URL Shortener and Analytics
-                <Header.Subheader>
-                  Shorten your URL and view some analytics.
-                </Header.Subheader>
-              </Header>
-              <Divider />
-              <Tabs />
-            </Segment>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    );
-  }
-}
-
-export default connect()(Welcome);
+export default Welcome;
