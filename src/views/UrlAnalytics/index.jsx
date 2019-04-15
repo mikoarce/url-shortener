621b71c4e0d0c6b22de43b4310eb122c64584202
marce <mikoarce@gmail.com>
2019-04-14 20:17:26 -0400

    Add controlled component functionality to placeholder

--- src/views/UrlAnalytics/index.jsx
+++ src/views/UrlAnalytics/index.jsx
@@ -2,6 +2,26 @@ import React from 'react';
 import { Segment, Input, Icon, Header } from 'semantic-ui-react';
 
 class UrlAnalytics extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      url: '',
+    };
+
+    this.handleGetUrlAnalytics = this.handleGetUrlAnalytics.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleGetUrlAnalytics() {
+    const { url } = this.state;
+    console.log(url);
+  }
+
+  handleChange(_, { name, value }) {
+    this.setState({[name]: value});
+  }
+
   render() {
     return (
       <Segment placeholder >
@@ -9,7 +29,13 @@ class UrlAnalytics extends React.PureComponent {
           <Icon name="exclamation" />
           Enter link below to fetch information.
         </Header>
-        <Input size="small" action={{ icon: 'search' }} placeholder="Enter URL" />
+        <Input
+          name="url"
+          size="small"
+          action={{ icon: 'search', onClick: this.handleGetUrlAnalytics }}
+          placeholder="Enter URL"
+          onChange={this.handleChange}
+        />
       </Segment>
     );
   }
