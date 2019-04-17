acf1cc6bf999d5ec71ec8879f5fd0e54f9d76b52
marce <mikoarce@gmail.com>
2019-04-17 12:18:52 -0400

    Fix dependency issues when running tests.

--- src/views/Welcome/index.jsx
+++ src/views/Welcome/index.jsx
@@ -5,22 +5,22 @@ import {
 import Tabs from './Tabs';
 
 const Welcome = () => (
-<Grid verticalAlign="middle" columns={1} centered>
-  <Grid.Row>
-    <Grid.Column>
-      <Segment>
-        <Header>
-          URL Shortener and Analytics
-          <Header.Subheader>
-            Shorten your URL and view mocked analytics.
-          </Header.Subheader>
-        </Header>
-        <Divider />
-        <Tabs />
-      </Segment>
-    </Grid.Column>
-  </Grid.Row>
-</Grid>
+  <Grid verticalAlign="middle" columns={1} centered>
+    <Grid.Row>
+      <Grid.Column>
+        <Segment>
+          <Header>
+            URL Shortener and Analytics
+            <Header.Subheader>
+              Shorten your URL and view mocked analytics.
+            </Header.Subheader>
+          </Header>
+          <Divider />
+          <Tabs />
+        </Segment>
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
 );
 
 export default Welcome;
